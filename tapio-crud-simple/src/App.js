9afdc4037fb0d0a7562f1c388bf4f7aeeb336db3
Barbara <barbara.branco.c@gmail.com>
2022-05-14 17:18:23 +0200

    add-feature-working

--- tapio-crud-simple/src/App.js
+++ tapio-crud-simple/src/App.js
@@ -1,4 +1,5 @@
 import User from './components/User';
+import AddUser from './components/AddUser';
 import './App.css';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -19,10 +20,41 @@ function App() {
     })
   }
 
+  const onAdd = async (title,body) => {
+    await fetch('https://jsonplaceholder.typicode.com/posts/', {
+      method:'POST',
+      body:JSON.stringify({
+        title:title,
+        body:body
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      }
+    })
+    .then((res) =>{
+      if(res.status !== 201){
+        return
+      }else{
+        return res.json();
+      }
+    })
+    .then((data) => {
+      setUsers((users) => [...users, data]);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
   console.log(users)
   return (
     <div className="App">
       <h3>React CRUD Using JSONplaceholder</h3>
+
+      <br />
+
+      <AddUser onAdd={onAdd}/>
+
       <div>
         {
           users.map((user) =>(
