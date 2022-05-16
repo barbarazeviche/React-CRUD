a088a3855596f73071658890fff3a3607d0f70a3
Barbara <barbara.branco.c@gmail.com>
2022-05-16 08:13:34 +0200

    last-fixes

--- tapio-crud-simple/src/components/Container.style.js
+++ tapio-crud-simple/src/components/Container.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
-    background-color: #dfe6e9;
+    background-color: turquoise;
     display:flex;
     justify-content:center;
     align-items:center;
--- tapio-crud-simple/src/components/PostContainer.style.js
+++ tapio-crud-simple/src/components/PostContainer.style.js
@@ -6,42 +6,13 @@ export const PostContainer = styled.div`
     width: 50%;
     margin: 100px auto;
     box-shadow: 0px 2px 38px rgba(0, 0, 0, 0.2);
-    height: 1000px;
-    &:after{
-       /*  content: ''; */
-        position: absolute;
-        left: auto;
-        background:none;
-        z-index: -1;  
-        width: 90%;
-        height: 10px;
-        top: 30px;
-        right:8px;
-        -webkit-transform: rotate(-3deg);
-        -moz-transform: rotate(-3deg);
-        -o-transform: rotate(-3deg);
-        -ms-transform: rotate(-3deg);
-        transform: rotate(-3deg);
-        -webkit-box-shadow: 0px -20px 36px 5px #295d92;
-        -moz-box-shadow: 0px -20px 36px 5px #295d92;
-        box-shadow: 0px -25px 35px 0px rgba(0,0,0,0.5);
-    }
-    &:before{
-        position: absolute;
-        left: auto;
-        background:none;
-        z-index: -1; 
-        width: 10px;
-        height: 95%;
-        top: 5px;
-        right:18px;
-        -webkit-transform: rotate(3deg);
-        -moz-transform: rotate(3deg);
-        -o-transform: rotate(3deg);
-        -ms-transform: rotate(3deg);
-        transform: rotate(3deg);
-        -webkit-box-shadow: 20px 0px 25px 5px #295d92;
-        -moz-box-shadow: 20px 0px 25px 5px #295d92;
-        box-shadow: 22px 0px 35px 0px rgba(0,0,0,0.5);  
-    }
-`
+    height: 1000px;  
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+    word-wrap: break-all;
+    overflow-wrap:break-word;
+    
+`;
